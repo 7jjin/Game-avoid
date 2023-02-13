b685dcb2fd9a81ad54b6055b8c90b420615b1c91
7jjin <wlsgud6305@naver.com>
2023-02-13 16:29:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 
 //size
-canvas.width = window.innerWidth -100;
-canvas.height = window.innerHeight -100;
+canvas.width = window.innerWidth-20 ;
+canvas.height = window.innerHeight-20;
 
 // 배경 화면
 ctx.fillStyle = '#12bbFF';
 ctx.fillRect(0,0,canvas.width, canvas.height);
-
-// ctx.fillStyle = 'black';
-// ctx.fillRect(10,10,50,50);
 
 let image1 = new Image();
 image1.src = "zhsks.jpg";
@@ -18,26 +15,27 @@ image1.src = "zhsks.jpg";
 let image2 = new Image();
 image2.src = "demo.png";
 
-// 캐릭터 Object
-let character = {
-    x: 10,      // x좌표
-    y: 200,     // y좌표
-    width: 50,  // 캐릭터 넓이
-    height: 50, // 캐릭터 높이
-    draw(){
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(image1,this.x,this.y,this.width,this.height);  // 이미지 가져오기
-    }
-}
-character.draw();   // 주인공 생성
-
 // 이미지 load후 draw
 function drawOnImage(image,x,y,w,h){
     image.onload = function(){
         ctx.drawImage(image,x,y,w,h);
     }
 }
+
+// 캐릭터 Object
+let character = {
+    x: 10,      // x좌표
+    y: canvas.height/2,     // y좌표
+    width: 50,  // 캐릭터 넓이
+    height: 50, // 캐릭터 높이
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(image1,this.x,this.y,this.width,this.height);  // 이미지 가져오기
+    }
+}
+
+
+
 
 //악당들
 class villain{
@@ -50,31 +48,31 @@ class villain{
     draw() {
         if(this.y<=100) this.y += 200;
         else if(this.y >= canvas.height -100) this.y -=500;
-        ctx.fillStyle = "red";
         ctx.fillRect(this.x,this.y,this.width,this.height);
         ctx.drawImage(image2,this.x,this.y,this.width,this.height);
     }
 }
-let v1 = new villain();
-v1.draw();      // 악당들 생성
+
 
 // 충돌 함수
 function checkCollision(character,villain){
     let collsion = false;
     if(
         (
-            (villain.x<character.x && character.x <= villain.x +50)||(villain.x<= character.x +50 && character.x +50 <= villain.x +50)
+            (villain.x<character.x && character.x <= villain.x +50)
+            ||
+            (villain.x<= character.x +50 && character.x +50 <= villain.x +50)
         )&&
         (
-            (villain.y<character.y && character.y <= villain.y +50)||(villain.y<= character.y +50 && character.y +50 <= villain.y +50)
+            (villain.y<character.y && character.y <= villain.y +50)
+            ||
+            (villain.y<= character.y +50 && character.y +50 <= villain.y +50)
         )
     )
     {
         collsion = true;
     }
     if(collsion){
-        ctx.fillStyle = "blue";
-        ctx.fillRect(villain.x-50,villain-50,villain.width+100, villain.height+100)
         ctx.fillStyle="yellow";
         ctx.fillRect(character.x-50,character.y-50,character.width+100, character.height+100)
         cancelAnimationFrame(animation);
@@ -90,52 +88,46 @@ let animation;
 let up,down,left,right;
 function frameAnimation(){
     animation = requestAnimationFrame(frameAnimation)
-    timer++;
+    timer++;        //1초에 60증가
 
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    ctx.fillStyle = '#12bbFF';
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
-
-    if(timer % 30 ===0){
+    // 악당이 생성되는 빈도 수
+    if(timer % 60 ===0){
         let villainNew = new villain();
         villains.push(villainNew);
     }
-    villains.forEach((object,index,array)=>{
-        if(object.x<0){
-            array.splice(index,1);
-        }
-        object.x-=2;
+    villains.forEach((item)=>{
+        item.x-=2;    // 적이 다가오는 속도
+        
+        checkCollision(character, item);
 
-        checkCollision(character, object);
-
-        object.draw();
+        item.draw();
     })
     if(up){
         up=false;
-        character.y -= 7;
+        character.y -= 20;
     }
     if(down){
         down = false;
-        character.y +=7;
+        character.y +=20;
     }
     if(left){
         left=false;
-        character.x -= 7;
+        character.x -= 20;
     }
     if(right){
         right = false;
-        character.x +=7;
+        character.x +=20;
     }
-
-
+    // 주인공 속도
     character.draw(); 
-    if(timer ===5000) cancelAnimationFrame(animation);
+    if(timer ===3600) cancelAnimationFrame(animation);  // 1분동안 실행 후 멈춤
 }
 frameAnimation();
 
+
 document.addEventListener("keydown",function(e){
-    console.log(e.code);
     if(e.code==="ArrowUp"){
         up=true;
     }else if(e.code==="ArrowDown"){
